3827477c7f3f656a1b9ee977201212c707b17fba
mattnorris1990 <mattnorris1990@gmail.com>
2022-07-10 14:18:26 +0100

    popovers for prompts now showing, styling begun

--- src/components/Prompt.js
+++ src/components/Prompt.js
@@ -1,10 +1,35 @@
 import React from "react"
+import { OverlayTrigger } from "react-bootstrap"
+import styled from "styled-components"
+import Popover from "react-bootstrap/Popover"
 
-const Prompt = ({prompt}) => {
+const StyledPop = styled.div`
+background-color: pink;
+`
+
+const ImagePrompt = styled.img`
+height : 7vh;
+`
+
+const Prompt = ({ prompt }) => {
+
+    const popover = (
+        <StyledPop>
+            <Popover id="popover-basic">
+                <Popover.Header as="h3">{prompt.name}</Popover.Header>
+                <Popover.Body>
+                    <p>{prompt.fact}</p>
+                </Popover.Body>
+            </Popover>
+        </StyledPop>
+    );
 
     return (
         <>
-            <p>{prompt.name}</p>
+            <OverlayTrigger trigger="click" placement="right" overlay={popover}>
+                <ImagePrompt src={`http://localhost:8080/${prompt.imageSrc}`} />
+            </OverlayTrigger>
+
         </>
     )
 }
