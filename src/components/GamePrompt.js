975401fa480ef044b42f9efbb4bd2e8426abcd58
mattnorris1990 <mattnorris1990@gmail.com>
2022-07-12 14:17:57 +0100

    first pass on styling finished

--- src/components/GamePrompt.js
+++ src/components/GamePrompt.js
@@ -1,14 +1,33 @@
 import React from "react"
+import styled from "styled-components"
+import { motion } from "framer-motion"
 
-const GamePrompt = ({prompt, setSelectedPrompt}) => {
+const PromptObj = styled.div`
+    max-height: 3vh;
+`
+
+
+const GamePrompt = ({ prompt, setSelectedPrompt }) => {
 
     const handleClick = () => {
         setSelectedPrompt(prompt.name)
     }
 
-    return(
+
+    return (
         <>
-            <img src={`http://localhost:8080/${prompt.imageSrc}`} onClick = {handleClick}/>
+            <PromptObj>
+                <motion.img
+                    src={`http://localhost:8080/${prompt.imageSrc}`}
+                    onClick={handleClick}
+                    whileHover={{ 
+                        rotate: [0, 0, 10, -10, 0],
+                        scale: 1.05
+                    }}
+                    style={{height: "10vh"}}
+                    
+                />
+            </PromptObj>
         </>
     )
 }
--- src/components/Goal.js
+++ src/components/Goal.js
@@ -1,14 +1,34 @@
 import React from "react"
+import styled from "styled-components"
+import box from "../assets/box.png"
+import {motion} from "framer-motion"
 
-const Goal = ({prompt, setSelectedGoal}) => {
+const GoalObj = styled.div`
+min-height: 5vh;
+`
+
+const Goal = ({ prompt, setSelectedGoal, completed }) => {
 
     const handleClick = () => {
         setSelectedGoal(prompt.name)
     }
 
-    return(
+    return (
         <>
-            <img src={`http://localhost:8080/${prompt.imageSrc}`} onClick = {handleClick}/>
+            <GoalObj onClick={handleClick}>
+                <motion.img
+                    src={box}
+                    height="50"
+                    whileHover={{
+                        rotate: [0, 0, 10, -10, 0],
+                        scale: 1.05
+                    }}
+                    style={{ height: "10vh" }}
+                />
+                <h2>{prompt.name}</h2>
+                {completed.includes(prompt.name) ? <p>Correct!</p> : null}
+            </GoalObj>
+
         </>
     )
 }
